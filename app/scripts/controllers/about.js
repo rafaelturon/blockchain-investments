546b0d2df6c8a58799fc907428d0124a7439a3aa
Rafael Turon Balbino <rafael.turon@gmail.com>
2015-12-31 12:28:33 -0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name expensePointApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the expensePointApp
+ */
+angular.module('expensePointApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name expensePointApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the expensePointApp
+ */
+angular.module('expensePointApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
