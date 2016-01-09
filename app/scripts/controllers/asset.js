c55bae768df060369143aae6f1ba99432b7567c0
rafaelturon <rafael.turon@gmail.com>
2016-01-09 16:27:50 -0200

    Bootstrap card deck

--- app/scripts/controllers/asset.js
+++ app/scripts/controllers/asset.js
@@ -29,6 +29,24 @@ angular.module('expensePointApp').controller('AssetCtrl', function ($scope, loca
       'value': 2564.50,
       'date': '2012-04-23T18:25:43.511Z',
       'percentage': 25
+    },
+    {
+      'name': 'Dollar',
+      'value': 1564.50,
+      'date': '2013-04-23T18:25:43.511Z',
+      'percentage': 34
+    },
+    {
+      'name': 'Euro',
+      'value': 3564.50,
+      'date': '2014-04-23T18:25:43.511Z',
+      'percentage': 41
+    },
+    {
+      'name': 'Yen',
+      'value': 2564.50,
+      'date': '2012-04-23T18:25:43.511Z',
+      'percentage': 25
     }
   ];
   
