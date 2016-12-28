2f8f1f8a3676031f58e30e1e03f3ab08e8169171
Rafael <rafael@turon>
2016-12-28 17:50:35 -0200

    Experimental Airbtiz login

--- src/Blockchain.Investments.Web/ClientApp/app/components/navmenu/navmenu.component.ts
+++ src/Blockchain.Investments.Web/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+declare var _abcUi: any;
+declare var _account: any;
 
 @Component({
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html',
     styleUrls: ['./navmenu.component.css']
 })
+
 export class NavMenuComponent {
+    login(event) {
+        if (_account === null) {
+            _abcUi.openLoginWindow(function(error, account) {
+                _account = account;
+            });
+        } else {
+            _account.logout();
+        }
+        // Account Id: _account.repoInfo.dataKey.toString('base64')
+    }
 }
